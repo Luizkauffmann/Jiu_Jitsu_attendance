af37a648e87aceeff43cef0edfe8c74c3e31f183
Luizkauffmann <54593267+Luizkauffmann@users.noreply.github.com>
2026-09-21 12:02:44 -0400

    Add js/config.js

---
+++ js/config.js
@@ -0,0 +1,5 @@
+window.APP_CONFIG = Object.freeze({
+  API_URL: "https://script.google.com/macros/s/AKfycbwPOVysFUR3hyrQ7h70ggcJYYj6je3JeSbh1XjMilKEo5rKG-VCSusohdIq9ShF0iYtFA/exec",
+  DEFAULT_CLASS_ID: "C001",
+  DEFAULT_CLASS_NAME: "Adults"
+});
